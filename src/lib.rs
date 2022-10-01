521ef8c0be9ae4195293da0c968dc02e5a98d60d
Richard Neumann <mail@richard-neumann.de>
2022-10-01 11:41:22 +0200

    Rename read() to read_string()

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::{BufReader, Error, Read};
 use std::path::Path;
 
-pub fn read(file: &Path) -> Result<String, Error> {
+pub fn read_string(file: &Path) -> Result<String, Error> {
     let fh = File::open(file)?;
     let mut buf_reader = BufReader::new(fh);
     let mut content = String::new();
